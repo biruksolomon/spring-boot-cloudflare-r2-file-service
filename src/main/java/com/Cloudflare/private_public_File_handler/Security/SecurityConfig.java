0280f8a721054e1847fc9f0e5bd1db06c0c07ecb
=biruksolomon <=biruksolomonmoges@gmail.com>
2025-12-17 10:18:24 +0300

    Configure the security for rolebased file handling

---
+++ src/main/java/com/Cloudflare/private_public_File_handler/Security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.Cloudflare.private_public_File_handler.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/files/public/**").permitAll()
+                        .requestMatchers("/files/private/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
