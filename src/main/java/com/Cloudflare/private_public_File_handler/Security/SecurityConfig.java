1079c956fd814f8a0420b552943719e136d7dc94
=biruksolomon <=biruksolomonmoges@gmail.com>
2025-12-17 20:25:40 +0300

    make configurable the Security Filter

--- src/main/java/com/Cloudflare/private_public_File_handler/Security/SecurityConfig.java
+++ src/main/java/com/Cloudflare/private_public_File_handler/Security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.Cloudflare.private_public_File_handler.Security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -8,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
