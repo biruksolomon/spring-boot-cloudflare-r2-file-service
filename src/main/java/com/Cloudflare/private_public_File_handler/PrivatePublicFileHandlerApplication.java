abcaa0bf16095ebf3865697b8b44b7fc238491a9
=biruksolomon <=biruksolomonmoges@gmail.com>
2025-12-17 09:44:14 +0300

    initalized simple cloudflare S3 storage integration sample for private and public file access

---
+++ src/main/java/com/Cloudflare/private_public_File_handler/PrivatePublicFileHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.Cloudflare.private_public_File_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatePublicFileHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivatePublicFileHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cloudflare/private_public_File_handler/PrivatePublicFileHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cloudflare.private_public_File_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivatePublicFileHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
